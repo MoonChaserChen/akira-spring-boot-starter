a772bb4740b4e91bc503882426c75fcd089d7305
雪行 <houzhang.chz@alibaba-inc.com>
2021-02-09 19:22:23 +0800

    limit script change

--- jedis-spring-boot-starter/src/main/resources/rate_limiter_init.lua
+++
@@ -1,8 +0,0 @@
-local exists = redis.call('exists', KEYS[1])
-if (exists == 1) then
-    return
-end
-
-local time_info = redis.call('TIME')
-local current_time = time_info[1] * 1000 + math.floor(time_info[2] / 1000)
-redis.call('HMSET', KEYS[1], 'last_time', current_time, 'current_token', ARGV[1], 'max_token', ARGV[1], 'token_rate', ARGV[2])
